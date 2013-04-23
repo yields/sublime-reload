05ba8e9f892a70924cb185c5a02cb47cb9ed5661
Amir Abu Shareb <yields@icloud.com>
2013-04-23 06:39:12 +0300

    only refresh if the browsers are running, inject style instead of browser refresh

--- __init__.py
+++ __init__.py
@@ -1,29 +1,52 @@
 import sublime, sublime_plugin
 from subprocess import call
+from subprocess import check_output
+
+# js to replace styles
+
+js = """
+  var links = document.getElementsByTagName('link');
+  var len = links.length;
+  for (var i = 0; i < len; ++i) {
+    if (style(links[i])) {
+      links[i].disabled = true;
+      links[i].disabled = false;
+    }
+  }
+
+  function style(el){
+    return 0 == el.getAttribute('rel').indexOf('style');
+  }
+"""
  
 class RefreshBrowsers(sublime_plugin.EventListener):
 
   # refresh
   def on_post_save_async(self, view):
-    self.chrome()
-    self.safari()
+    if self.running('Google Chrome'):
+      self.chrome()
+    if self.running('Safari'):
+      self.safari()
+
+  # chech if `app` is running.
+  def running(self, app):
+    out = check_output(['osascript', '-e', """
+      tell app "System Events" to count processes whose displayed name is "{app}"
+    """.format(app=app)])
+    return 0 < int(out)
 
   # chrome
   def chrome(self):
     call(['osascript', '-e', """
-        tell application "chrome"
-            set winref to a reference to (first window whose title does not start with "Developer Tools -")    
-            set winref's index to 1
-            reload active tab of winref
-        end tell 
-    """])
+      tell application "Google Chrome"
+        execute front window's active tab javascript "{js}"
+      end tell
+    """.format(js=js)])
 
   # safari
   def safari(self):
     call(['osascript', '-e', """
-        tell application "safari"
-            tell its first document
-                set its URL to (get its URL)
-            end tell
-        end tell
-    """])
+      tell application "Safari" to do JavaScript "{js}" in document 1
+    """.format(js=js)])
+
+    
